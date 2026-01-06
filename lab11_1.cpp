96c272b6e2d3bc6755a3cabe48905a7dbf5b1439
nicklovecat <skykillergamer@gmail.com>
2026-01-06 13:42:37 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,23 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}
